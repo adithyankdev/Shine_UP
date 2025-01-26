3e64ce6e992fea1fabff77fa419072e18f31dca0
adithyank02 <148231878+adithyankdev@users.noreply.github.com>
2025-01-26 12:41:23 +0530

    Obstacles Developing

--- Source/ShineUp_2D/Private/Obstacle/Derived/MovableObstacle.cpp
+++ Source/ShineUp_2D/Private/Obstacle/Derived/MovableObstacle.cpp
@@ -14,10 +14,7 @@ AMovableObstacle::AMovableObstacle()
 	BaseCollider = CreateDefaultSubobject<UCapsuleComponent>("Capsule Collision");
 	BaseCollider->SetupAttachment(RootComponent);
 
-	DirectionalArrow = CreateDefaultSubobject<UArrowComponent>("Arrow");
-	DirectionalArrow->SetupAttachment(RootComponent);
 
-	ChangeInTime = ConstantInterval;
 }
 
 void AMovableObstacle::BeginPlay()
@@ -27,53 +24,10 @@ void AMovableObstacle::BeginPlay()
 	//Binding The Delegate And Function
 	BaseCollider->OnComponentBeginOverlap.AddDynamic(this, &AMovableObstacle::OnBeginOverlap);
 
-	//Triggering The TrunTimer Function For Counting Down
-	StartTurnTimer();
-
-}
-
-void AMovableObstacle::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-	//Moving The Actor Frame By Frame
-	AddActorLocalOffset(((DirectionalArrow->GetRightVector())*Speed) * DeltaTime);
-
 }
 
 void AMovableObstacle::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	OnActorCollisionOverlap(OtherActor);
-}
-
-void AMovableObstacle::StartTurnTimer()
-{
-	//Repeatadly Calling On Every Second For Decrementing The Count
-	GetWorld()->GetTimerManager().SetTimer(TurnTimerHandle, this, &AMovableObstacle::UpdateTunrTimer, 1, true);
-}
-
-void AMovableObstacle::UpdateTunrTimer()
-{
-	ChangeInTime--;
-	if (ChangeInTime == 0)
-	{
-		GetWorld()->GetTimerManager().ClearTimer(TurnTimerHandle);
-
-        //Turing The Actor
-		FRotator CurrentRotation = GetActorRotation();
-		CurrentRotation.Yaw += 180.0f; // Add 180 degrees to the current Z-axis rotation (Yaw)
-		SetActorRotation(CurrentRotation);
-
-		OnTurnTimerRunsOut();
-	}
-}
-
-//Function To Re-start The Timer When It Turn Out To Zero
-void AMovableObstacle::OnTurnTimerRunsOut()
-{
-	//Reset Timer
-	ChangeInTime = ConstantInterval;
-	
-	StartTurnTimer();
 }
 
--- Source/ShineUp_2D/Public/Obstacle/Derived/MovableObstacle.h
+++ Source/ShineUp_2D/Public/Obstacle/Derived/MovableObstacle.h
@@ -28,27 +28,12 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Collider")
 	UCapsuleComponent* BaseCollider;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Base")
-	UArrowComponent* DirectionalArrow;
-
 	void BeginPlay() override;
 
-	void Tick(float DeltaTime)override;
-
+	
 	//Function That Triggers On Overlap Event
 	UFUNCTION()
 	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
-	int const ConstantInterval = 5.0f; 
-	int ChangeInTime;
-
-	float const Speed = -100.0f;
-
-
-	FTimerHandle TurnTimerHandle;
-
-	void StartTurnTimer();
-	void UpdateTunrTimer();
-	void OnTurnTimerRunsOut();
 
 };
